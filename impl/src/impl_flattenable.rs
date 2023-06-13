3f201e24c4315b3559130a63e66c0c02bc115ef4
bradlet <bthompson@tradewelltech.co>
2023-06-12 20:53:45 -0700

    Implement Flattenable and realize it won't help my problem

--- impl/src/impl_flattenable.rs
+++ impl/src/impl_flattenable.rs
@@ -1,4 +1,6 @@
+use itertools::{Either, Itertools};
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::{Data::Struct, DeriveInput, Fields::Named};
 
@@ -9,18 +11,33 @@ use syn::{Data::Struct, DeriveInput, Fields::Named};
 pub fn flatten(input: &DeriveInput) -> TokenStream {
     let name = &input.ident;
 
-    if let Struct(derived) = &input.data {
+    let (flattenable_fields, other_fields) = if let Struct(derived) = &input.data {
         if let Named(fs) = &derived.fields {
-            fs.named.iter().for_each(|f| {
-                eprintln!("FLATTEN type: {:?} | attrs: {:?}", &f.ty, &f.attrs);
+            fs.named.iter().partition_map(|f| {
+                // eprintln!("FLATTEN type: {:?} | attrs: {:?}", &f.ty, &f.attrs);
+                // Check for fields with the `#[flattenable]` attribute
+                if f.attrs.len() == 1
+                    && Into::<&Ident>::into(f.attrs.first().unwrap().path().get_ident().unwrap())
+                        == &Ident::new("flattenable", Span::call_site())
+                {
+                    Either::Left(f.clone())
+                } else {
+                    Either::Right(f.clone())
+                }
             })
+        } else {
+            (vec![], vec![])
         }
+    } else {
+        (vec![], vec![])
     };
 
+    // Any fields that need to be flattened will be appended to the fields that
+    // don't need to be flattened. This is not a very efficient macro!
     let gen = quote! {
         impl Flattenable for #name {
             fn flatten() -> Vec::<syn::Field> {
-                vec![]
+                vec![#(#other_fields),*]#(.append(#flattenable_fields.flatten()))*
             }
         }
     };
